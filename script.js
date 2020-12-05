f4e8054356c2a4a237bf2659007e1fac612a0028
Austin Ruhf <austin.ruhf@gmail.com>
2020-12-04 22:12:49 -0500

    created initial prompts

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+   // var passwordText = document.querySelector("#password");
+    var passwordLength = prompt("How long will your password be? Choose between 8 and 128 characters.")
+
+        if (passwordLength > 128) {
+         alert("Please choose a number between 8 and 128.");
+            return;
+    }
+        else if (passwordLength < 8) {
+            alert("Please choose a number between 8 and 128");
+            return;
+        }
+}
